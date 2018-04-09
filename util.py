90cb86e41dc71a1863e55171e871379d21885813
sirius.zhang <sirius.zhang@weshareholdings.com>
2018-04-09 15:22:34 +0800

    实现保存聊天记录的基本功能

---
+++ util.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+MSG_TYPE_TEXT = 'Text'
+DB_NAME = 'wxRobot'
--- wxRobot.py
+++ wxRobot.py
@@ -1,32 +1,62 @@
 # -*- coding: utf-8 -*-
 import itchat
 from itchat.content import *
+import pymongo
+from util import *
+from bot import *
 
-@itchat.msg_register(itchat.content.TEXT)
-def text_reply(msg):
-    print(msg)
-    # return chatbot.get_response(msg['Content']).text
+class App(object):
+    """全局唯一进程实例"""
+    def __init__(self):
+        super(App, self).__init__()
+        self.itchat = itchat
+        self.wx_robot_conn = pymongo.MongoClient(host='127.0.0.1',port=27017)
 
-@itchat.msg_register(TEXT, isGroupChat=True)
-def text_reply(msg):
-    print(msg)
-    # return chatbot.get_response(msg['Content']).text
+        #所有的单独对话,比如 filehelper(文件助手),公众号,单个好友
+        @self.itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING], isFriendChat = True)
+            self.wx_robot_conn.db[DB_NAME].insert(msg)
+            self.itchat.send(self.get_response(msg['Content']), msg['FromUserName'])
 
-@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
-def download_files(msg):
-    print(msg)
-    return "download_files"
-    # msg.download(msg.fileName)
-    # typeSymbol = {
-    #     PICTURE: 'img',
-    #     VIDEO: 'vid', }.get(msg.type, 'fil')
-    # return '@%s@%s' % (typeSymbol, msg.fileName)
+        @self.itchat.msg_register(FRIENDS, isFriendChat = True)
+        def addFriendChat(msg):
+            self.itchat.send(self.get_response(msg['Content']), msg['FromUserName'])
 
-@itchat.msg_register(FRIENDS)
-def add_friend(msg):
-    return  "Hello"
+        @self.itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
+        def download_files(msg):
+            FileName = msg['FileName']
+            download_fun = msg['Text']
+            with open(FileName, 'wb') as f:
+                f.write(download_fun())
 
-    # msg.user.send(u'@%s\u2005I received: %s' % (msg.actualNickName, msg.text))
+        #群聊天
+        @self.itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING], isGroupChat = True)
+        def GroupChat(msg):
+            self.wx_robot_conn.db[DB_NAME].insert(msg)
+            if self.at_robot_string in msg['Content']:
+                if "@@" in msg['FromUserName']:
+                    self.itchat.send(self.get_response(msg['Content']), msg['FromUserName'])
+                elif "@@" in msg['ToUserName']:
+                    self.itchat.send(self.get_response(msg['Content']), msg['ToUserName'])
 
-itchat.auto_login(enableCmdQR=False, hotReload=True)
-itchat.run()
+        #
+        @self.itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING], isMpChat = True)
+        def MpChat(msg):
+            self.wx_robot_conn.db[DB_NAME].insert(msg)
+
+        self.bot = Bot()
+
+    def get_response(self, text):
+        return self.bot.get_response(text)
+
+    def run(self):
+        try:
+            self.bot.Train()
+            self.itchat.auto_login(enableCmdQR=False, hotReload=True)
+            self.at_robot_string = '@' + self.itchat.search_friends()['PYQuanPin']
+            self.itchat.run()
+        except Exception as e:
+            self.itchat.logout()
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
